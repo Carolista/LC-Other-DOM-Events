2f8b15fe1be38121cfcf9fdacb7f6a2b63d0b3d4
Carolista <cjones.renaissancewoman@gmail.com>
2020-10-27 15:55:22 -0500

    added files

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/***      ***/
+
+/*
+    
+*/
+
+// Event listener for page load
+window.addEventListener("load", function() {
+    console.log('Page loaded.');
+    init();
+});
+
+// DOM code for page elements
+function init() {
+
+
+
+
+}
