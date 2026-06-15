a9e092b159db9ac186a028c532148da753f711ad
Carolista <cjones.renaissancewoman@gmail.com>
2026-06-15 08:19:11 -0400

    updated solution with modern syntax

--- scripts.js
+++ scripts.js
@@ -35,44 +35,44 @@ function toggleColor(element) {
 /** EVENT HANDLERS **/
 
 // When the "click me" button is clicked
-clickButton.addEventListener('click', function () {
+clickButton.addEventListener('click', () => {
 	clickButton.style.backgroundColor = toggleColor(clickButton);
 });
 
 // When the "double-click me" button is double-clicked
-doubleClickButton.addEventListener('dblclick', function () {
+doubleClickButton.addEventListener('dblclick', () => {
 	doubleClickButton.style.backgroundColor = toggleColor(doubleClickButton);
 });
 
 // When the mouse first passes *onto* the "pass over me" button
-passOverButton.addEventListener('mouseover', function () {
+passOverButton.addEventListener('mouseover', () => {
 	passOverButton.style.backgroundColor = ALT_COLOR;
 });
 
 // When the mouse passes *off of* the "pass over me" button
-passOverButton.addEventListener('mouseout', function () {
+passOverButton.addEventListener('mouseout', () => {
 	passOverButton.style.backgroundColor = BASE_COLOR;
 });
 
 // When the mouse first passes *onto* the "and me" button
-andMeButton.addEventListener('mouseover', function () {
+andMeButton.addEventListener('mouseover', () => {
 	passOverButton.style.backgroundColor = ALT_COLOR;
 	andMeButton.style.backgroundColor = ALT_COLOR;
 });
 
 // When the mouse passes *off of* the "and me" button
-andMeButton.addEventListener('mouseout', function () {
+andMeButton.addEventListener('mouseout', () => {
 	andMeButton.style.backgroundColor = BASE_COLOR;
 	passOverButton.style.backgroundColor = BASE_COLOR;
 });
 
 // When the mouse presses down on the "hold me down" button
-pressDownButton.addEventListener('mousedown', function () {
+pressDownButton.addEventListener('mousedown', () => {
 	pressDownButton.style.backgroundColor = ALT_COLOR;
 });
 
 // When the mouse presses down on the "hold me down" button
-pressDownButton.addEventListener('mouseup', function () {
+pressDownButton.addEventListener('mouseup', () => {
 	pressDownButton.style.backgroundColor = BASE_COLOR;
 });
 
@@ -84,40 +84,9 @@ let y = 0;
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-const clearCanvas = document.getElementById('clear');
+const clearButton = document.getElementById('clear');
 
-// When the user presses down with the mouse
-canvas.addEventListener('mousedown', e => {
-	x = e.offsetX;
-	y = e.offsetY;
-	isDrawing = true;
-});
-
-// When the user moves the mouse after pressing down
-canvas.addEventListener('mousemove', e => {
-	if (isDrawing === true) {
-		drawLine(context, x, y, e.offsetX, e.offsetY);
-		x = e.offsetX;
-		y = e.offsetY;
-	}
-});
-
-// When the user releases the mouse
-window.addEventListener('mouseup', e => {
-	if (isDrawing === true) {
-		drawLine(context, x, y, e.offsetX, e.offsetY);
-		x = 0;
-		y = 0;
-		isDrawing = false;
-	}
-});
-
-// When the user presses the "clear canvas" button
-clearCanvas.addEventListener('click', e => {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-});
-
-// Needed for mousemove handler above
+// Needed for mousemove and mouseup handlers
 function drawLine(context, x1, y1, x2, y2) {
 	context.beginPath();
 	context.strokeStyle = 'black';
@@ -127,3 +96,34 @@ function drawLine(context, x1, y1, x2, y2) {
 	context.stroke();
 	context.closePath();
 }
+
+// When the user presses down with the mouse
+canvas.addEventListener('mousedown', event => {
+	x = event.offsetX;
+	y = event.offsetY;
+	isDrawing = true;
+});
+
+// When the user moves the mouse after pressing down
+canvas.addEventListener('mousemove', event => {
+	if (isDrawing === true) {
+		drawLine(context, x, y, event.offsetX, event.offsetY);
+		x = event.offsetX;
+		y = event.offsetY;
+	}
+});
+
+// When the user releases the mouse
+window.addEventListener('mouseup', event => {
+	if (isDrawing === true) {
+		drawLine(context, x, y, event.offsetX, event.offsetY);
+		x = 0;
+		y = 0;
+		isDrawing = false;
+	}
+});
+
+// When the user presses the "clear canvas" button
+clearButton.addEventListener('click', () => {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+});
